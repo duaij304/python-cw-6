5b938cb1771d37f9a0173acdf75fb3084049d30c
duaij304 <69263437+duaij304@users.noreply.github.com>
2023-09-06 18:58:53 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # write your code here
+
+person={
+    'name':'Duaij',
+    'age':'17',
+    'hobbies':['Programing','Pentration testing','football']
+}
+print(person['name'])
+print(person['age'])
+
+person['age']=17
+person['country']='Kuwait'
+print(person)
+print(len(person))
+
+person['hobbies'].append('reading')
+
+def check_hobby(person):
+    if len(person['hobbies']) >= 3:
+        print('WOW you are amazing!')
+
+check_hobby(person)
